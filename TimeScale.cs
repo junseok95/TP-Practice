a337d32b63ba2422b7b2ad8f1aa4ce60118a9167
junseok95 <37564074+junseok95@users.noreply.github.com>
2018-04-23 16:26:29 +0900

    타임스케일
    순간적으로 느려지거나 빨라지게끔 하는 함수

---
+++ TimeScale.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeScale : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	//타임스케일
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey("n"))
+			Time.timeScale = 0.03f;
+		else if (Input.GetKeyUp("n"))
+			Time.timeScale = 1.0f;
+	}
+}
