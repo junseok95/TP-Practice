3fd5b6c3de20821c576a8c14ede09a8f79f25115
junseok95 <37564074+junseok95@users.noreply.github.com>
2018-04-23 16:27:01 +0900

    코루틴
    일정시간 프레임만큼 적용되는 함수

---
+++ Coroutine.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coroutine : MonoBehaviour {
+
+	[SerializeField]
+	private int nFrames = 100;
+
+	[SerializeField]
+	private float slowDownScale = 0.03f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	//코루틴
+	IEnumerator slow(int n_frames)
+	{
+		Time.timeScale = slowDownScale;
+		while (n_frames > 0)
+		{
+			n_frames--;
+			yield return new WaitForEndOfFrame();
+		}
+		Time.timeScale = 1.0f;
+	}
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("n"))
+		{
+			StartCoroutine(slow(30));
+		}
+	}
+}
