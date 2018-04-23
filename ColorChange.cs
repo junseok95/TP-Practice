fda28a05468cf7e77af24b4f6bc091553da7f90f
junseok95 <37564074+junseok95@users.noreply.github.com>
2018-04-23 15:53:56 +0900

    색변경하기
    키입력

---
+++ ColorChange.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChange : MonoBehaviour
+{
+
+	private MeshRenderer mr;
+	// Use this for initialization
+	void Start()
+	{
+		mr = GetComponent<MeshRenderer>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if(Input.GetKeyDown("r"))
+		mr.material.color = Color.red;
+	}
+}
