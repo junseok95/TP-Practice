eb928f05729e4734b57c3d983d9c992c593a1b72
junseok95 <37564074+junseok95@users.noreply.github.com>
2018-11-20 12:48:29 +0900

    Add files via upload

---
+++ MouseRotateControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseRotateControl : MonoBehaviour {
+
+    Rotate rot;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseDown()
+    {
+        print("1");
+        if(rot == null)
+        {
+            rot = gameObject.AddComponent<Rotate>();
+        }
+        else
+        {
+            Destroy(rot);
+            rot = null;
+        }
+    }
+}
